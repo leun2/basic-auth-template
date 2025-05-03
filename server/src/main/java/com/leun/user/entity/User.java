1a0332e88c6ad588e12aed84aa6cead70850120c
leun <leun12968@gmail.com>
2025-05-03 21:40:09 +0900

    feat: add User entity

---
+++ server/src/main/java/com/leun/user/entity/User.java
@@ -0,0 +1,72 @@
+package com.leun.user.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users")
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProviderType provider;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole userRole;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserProfile userProfile;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserSetting userSetting;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(String email, String password, ProviderType provider, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.provider = provider;
+        this.userRole = userRole;
+    }
+
+    public enum ProviderType {
+        LOCAL,
+        GOOGLE,
+        NAVER;
+    }
+
+    public enum UserRole {
+        ROLE_USER,
+        ROLE_ADMIN;
+    }
+}
