55bbcf4faf3e7a26b33e662273a23e3b1c00c0bd
leun <leun12968@gmail.com>
2025-05-03 21:40:16 +0900

    feat: add UserProfile entity

---
+++ server/src/main/java/com/leun/user/entity/UserProfile.java
@@ -0,0 +1,34 @@
+package com.leun.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    private String name;
+
+    private String image;
+
+    public UserProfile(User user, String name, String image) {
+        this.user = user;
+        this.name = name;
+        this.image = image;
+    }
+}
