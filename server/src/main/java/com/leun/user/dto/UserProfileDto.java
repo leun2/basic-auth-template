3d8135dd28d0aad83f4d44e784902feb5d699fc0
leun <leun12968@gmail.com>
2025-05-09 03:47:43 +0900

    feat: add UserProfileDto for name and image update with multipart support

---
+++ server/src/main/java/com/leun/user/dto/UserProfileDto.java
@@ -0,0 +1,35 @@
+package com.leun.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+public class UserProfileDto {
+    @Getter
+    @Setter
+    public static class Response {
+        private String email;
+        private String name;
+        private String image;
+
+        public Response(String name, String image) {
+            this.name = name;
+            this.image = image;
+        }
+    }
+
+    public static class Request {
+
+        @Getter
+        @Setter
+        public static class Name {
+            private String name;
+        }
+
+        @Getter
+        @Setter
+        public static class Image {
+            private MultipartFile image;
+        }
+    }
+}
