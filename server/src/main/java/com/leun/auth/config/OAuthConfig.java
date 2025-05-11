5f3ff24b550220ba9b650f33c230f15c73b6266d
leun <leun12968@gmail.com>
2025-05-11 22:02:25 +0900

    feat: add Naver OAuth configuration properties to OAuthConfig

--- server/src/main/java/com/leun/auth/config/OAuthConfig.java
+++ server/src/main/java/com/leun/auth/config/OAuthConfig.java
@@ -9,11 +9,26 @@ import org.springframework.context.annotation.Configuration;
 public class OAuthConfig {
 
     @Value("${spring.security.oauth2.client.registration.google.client-id}")
-    private String clientId;
+    private String googleClientId;
 
     @Value("${spring.security.oauth2.client.registration.google.client-secret}")
-    private String clientSecret;
+    private String googleClientSecret;
 
     @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
-    private String redirectUri;
+    private String googleRedirectUri;
+
+    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
+    private String naverClientId;
+
+    @Value("${spring.security.oauth2.client.registration.naver.client-secret}")
+    private String naverClientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
+    private String naverRedirectUri;
+
+    @Value("${spring.security.oauth2.client.registration.naver.token-uri}")
+    private String naverTokenUri;
+
+    @Value("${spring.security.oauth2.client.registration.naver.user-info-uri}")
+    private String naverUserInfoUri;
 }
