06954705428f471e326b27a0bf0f4d2a4c6b2352
leun <leun12968@gmail.com>
2025-06-04 23:55:45 +0900

    feat: add loading state to AuthenticatedRoute

--- client/src/components/auth/AuthenticatedRoute.tsx
+++ client/src/components/auth/AuthenticatedRoute.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { useAuth } from "components/auth/AuthContext"
 import { Navigate } from "react-router-dom";
+import LoadingSpinner from "components/common/LoadingSpinner";
 
 function AuthenticatedRoute({ children }: { children: React.ReactNode }) {
     const { authState } = useAuth();
-    return authState.isAuthenticated ? <>{children}</> : <Navigate to="/signin" />;
+
+    if (authState.isLoading) {
+        return <div><LoadingSpinner /></div>; // 또는 <LoadingSpinner /> 컴포넌트
+    }
+
+    // 2. 로딩이 끝났고, 인증되었다면 children을 렌더링합니다.
+    if (authState.isAuthenticated) {
+        return <>{children}</>;
+    }
+
+    // 3. 로딩이 끝났고, 인증되지 않았다면 로그인 페이지로 리디렉션합니다.
+    return <Navigate to="/signin" />;
 }
 
 export default AuthenticatedRoute;
