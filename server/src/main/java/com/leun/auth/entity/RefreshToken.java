189513128a6391f18b086f5d40029aa229fcf0f1
leun <leun12968@gmail.com>
2025-06-04 23:37:56 +0900

    feat: add RefreshToken entity with one-to-one mapping to User

---
+++ server/src/main/java/com/leun/auth/entity/RefreshToken.java
@@ -0,0 +1,43 @@
+package com.leun.auth.entity;
+
+
+import com.leun.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false, length = 500)
+    private String token;
+
+    public RefreshToken(User user, String token) {
+        this.user = user;
+        this.token = token;
+    }
+
+    public void updateToken(String newToken) {
+        this.token = newToken;
+    }
+}
